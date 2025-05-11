7e8e86f32403e8cca3126983bc3ec4b7be2e00b4
SawyerWetson <sawyer.wetson@gmail.com>
2025-05-11 17:50:22 -0500

    Update Restart.py

--- Restart.py
+++ Restart.py
@@ -1,6 +1,27 @@
-import Build.html
 import os
-if running = True:
-  print("Program is Running")
-if running = False:
- os.system("git reboot SawyerWetson/SmartUmpires")
+import time
+import requests
+
+def is_build_html_working():
+  try:
+    reponse = 
+requests.get("http://SawyerWetson/SmartUmpires/main/Build.html/
+       return response.status_code == 200
+      except requests.exceptions.RequestException:
+       return False:
+
+
+
+
+While True:
+ if is_build_html_working():
+  print("Build.html is Working. Checking Again in 10 seconds.....")
+   time.sleep(10)
+  else:
+      print("Build.html is not working Rebooting....")
+      os.system("git reset -- hard && git pull SawyerWetson/SmartUmpires")
+      os.system("start Build.html")
+
+      time.sleep(5
+  
+     
