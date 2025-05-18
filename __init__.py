4688cf971dea7fc8797a6454a247a6c7909c7dde
SawyerWetson <sawyer.wetson@gmail.com>
2025-05-18 18:19:15 -0500

    Improve  __init__.py to reference all of the files and version

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-___version__ == "1.0"
+___version__ = "1.0"
 
 def openingMessage():
   print("SmartUmpires Installed! Welcome!")
@@ -10,12 +10,12 @@ class SystemInfo:
  def __init__(self, system):
     self.name = name
     self.system = system
-
+def __str__(self):
+  return f"{self.name} ({self.system})"
 
 SysData = SystemInfo("Smart Umpires", "Github Repository SawyerWetson/SmartUmpires")
 print(f"System Info: {SysData}")
 
 if __name__ == "__main__":
   openingMessage()
-  __init__()
   
