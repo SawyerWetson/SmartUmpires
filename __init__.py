6aec8a6ea037d3c21c632eaed0032c435b848758
SawyerWetson <sawyer.wetson@gmail.com>
2025-05-18 18:12:13 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,16 @@
+___version__ == "1.0"
+
+def openingMessage():
+  print("SmartUmpires Installed! Welcome!)
+
+        
+        
+        
+class __init1__:
+ def __init__(self, system):
+    self.name = name
+    self.system = system
+
+
+ SysData = __init1__("Smart Umpires", "Github Repository SawyerWetson/SmartUmpires")
+ print(f"System Info: {SysData}")
