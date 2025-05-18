700c9e6866cf4d8585926c0a60339b72d50fb6c3
SawyerWetson <sawyer.wetson@gmail.com>
2025-05-18 18:16:06 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,21 @@
 ___version__ == "1.0"
 
 def openingMessage():
-  print("SmartUmpires Installed! Welcome!)
+  print("SmartUmpires Installed! Welcome!")
 
         
         
         
-class __init1__:
+class SystemInfo:
  def __init__(self, system):
     self.name = name
     self.system = system
 
 
-SysData = __init1__("Smart Umpires", "Github Repository SawyerWetson/SmartUmpires")
+SysData = SystemInfo("Smart Umpires", "Github Repository SawyerWetson/SmartUmpires")
 print(f"System Info: {SysData}")
+
+if __name__ == "__main__":
+  openingMessage()
+  __init__()
+  
