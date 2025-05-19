539c057fe72dce76ee6c3fda7af50ee36cf89871
SawyerWetson <sawyer.wetson@gmail.com>
2025-05-18 20:32:41 -0500

    Create test.java

---
+++ test.java
@@ -0,0 +1,6 @@
+public class test {
+  public static void main(String args[]) {
+    String Numbers = "44";
+    int NewNumbers = Integer.parseInt(Numbers);
+    double AddedNumbers = NewNumbers + 1.0;
+    
