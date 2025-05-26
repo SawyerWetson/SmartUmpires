0f732837d9b8c02bd8b0bbdb0dd3a55615cadfa6
SawyerWetson <sawyer.wetson@gmail.com>
2025-05-26 13:57:50 -0500

    Create c# test

---
+++ testing/test.cs
@@ -0,0 +1,16 @@
+   
+   
+   
+using system;
+namespace Test;
+{
+    class Main {
+        static void Main(string[] args) {
+            double number1 = 40.40
+            Console.WriteLine("Test Test");
+            string testVariable1 = Console.ReadLine();
+            int testVariable2 = Convert.ToInt32(Console.ReadLine());
+        
+    
+        
+    }
