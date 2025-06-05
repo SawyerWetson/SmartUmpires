cd0ffea9205eceac2c828276c6c4d9b5940c2699
TheMagicHatCoder <sawyer.wetson@gmail.com>
2025-06-05 11:21:44 -0500

    #1 Define SERVER_ADMIN with isset

--- Backend/server__init__.php
+++ Backend/server__init__.php
@@ -1,17 +1,17 @@
 <?php
- echo $_SERVER['SCRIPT_NAME'];
- echo $_SERVER['SERVER_ADMIN'];
- class System {
-   public $name = "SmartUmpires repo";
-   public $type = "Git repository";
+echo $_SERVER['SCRIPT_NAME'] . "<br>";
+echo (isset($_SERVER['SERVER_ADMIN']) ? $_SERVER['SERVER_ADMIN'] : '') . "<br>";
 
-   public function __construct($name, $type) {
-     $this->name = $name;
-     $this->type = $type;
-   }
- }
-$SmartUmpires = new System("SmartUmpires", "SmartUmpires'System");
-echo $SmartUmpires -> name;
-echo $SmartUmpires -> type;
+class System {
+  public $name = "SmartUmpires repo";
+  public $type = "Git repository";
 
-   
+  public function __construct($name, $type) {
+    $this->name = $name;
+    $this->type = $type;
+  }
+}
+
+$SmartUmpires = new System("SmartUmpires", "SmartUmpires System");
+echo $SmartUmpires->name . "<br>";
+echo $SmartUmpires->type . "<br>";
