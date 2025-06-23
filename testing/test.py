9d7695ab041c4a45c7c1c236646fb5678a39f875
TheMagicHatCoder <sawyer.wetson@gmail.com>
2025-06-23 11:37:01 -0500

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -1,28 +1,23 @@
-#!/bin/python
+#!/usr/bin/env python
 import os
 import time
 import requests
 
 def is_setup_py_working():
-  try:
-    reponse = 
-requests.get("https:/SawyerWetson/SmartUmpires/main/src/setup.py/"
-       return response.status_code == 200
-      except requests.exceptions.RequestException:
-       return False:
+    try:
+        response = requests.get(
+            "https://raw.githubusercontent.com/SawyerWetson/SmartUmpires/main/src/setup.py"
+        )
+        return response.status_code == 200
+    except requests.exceptions.RequestException:
+        return False
 
-
-
-
-While True:
- if is_build_html_working():
-  print("Build.html is Working. Checking Again in 10 seconds.....")
-   time.sleep(10)
-  else:
-      print("Build.html is not working Rebooting....")
-      os.system("git reset -- hard && git pull SawyerWetson/SmartUmpires")
-      os.system("start Build.html")
-
-      time.sleep(5)
-  
-     
+while True:
+    if is_setup_py_working():
+        print("setup.py is accessible. Checking again in 10 seconds.....")
+        time.sleep(10)
+    else:
+        print("setup.py is not accessible. Rebooting....")
+        os.system("git reset --hard && git pull SawyerWetson main")
+        os.system("start Build.html")  # This only works on Windows with proper associations
+        time.sleep(5)
