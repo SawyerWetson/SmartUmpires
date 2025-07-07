c587047f9b3cc4dc2d9f47f4bda74931652163a0
TheMagicHatCoder <sawyer.wetson@gmail.com>
2025-07-07 10:54:04 -0500

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import subprocess
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/run-strikezone', methods=['POST'])
 def run_strikezone():
