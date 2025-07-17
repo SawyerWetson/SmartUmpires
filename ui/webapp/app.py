bad178f67ef2e18650c572604637420151573b00
TheMagicHatCoder <sawyer.wetson@gmail.com>
2025-07-17 17:51:11 -0500

    Update app.py

--- ui/webapp/app.py
+++ ui/webapp/app.py
@@ -4,9 +4,12 @@ import sys
 import os
 
 app = Flask(__name__)
-app.route('/run-strikezone', methods=['POST'])
+
+@app.route("/")
 def home():
-    app.route("/")
+    return "SmartUmpires Flask Server is running!"
+
+@app.route('/run-strikezone', methods=['POST'])
 def run_strikezone():
     try:
         # Go up two levels: from ui/webapp/ to SmartUmpires/
@@ -21,5 +24,6 @@ def run_strikezone():
             return jsonify({'message': f'Error running setup.py:\n{output}'}), 500
     except Exception as e:
         return jsonify({'message': f'Exception: {e}'}), 500
+
 if __name__ == "__main__":
     app.run(debug=True)
